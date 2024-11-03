d2b186cdd3fc7dd7cb8cab2c9294ca7264819763
zemaf <enaccf31400@gmail.com>
2024-11-03 21:41:39 +0100

    templates + liens vers  accueil et création séjour créés, css à revoir

--- src/soignemoiwebsite/urls.py
+++ src/soignemoiwebsite/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import SoigneMoiWebsiteView
+from .views import SoigneMoiWebsiteView, CreerSejourView
 
 
 app_name = 'soignemoiwebsite'
 
 urlpatterns = [
     path('accueil/', SoigneMoiWebsiteView.as_view(), name='home'),
+    path('creer_sejour/', CreerSejourView.as_view(), name='creer_sejour'),
 ]
--- src/soignemoiwebsite/views.py
+++ src/soignemoiwebsite/views.py
@@ -5,3 +5,7 @@ from django.views.generic import TemplateView
 
 class SoigneMoiWebsiteView(TemplateView):
     template_name = 'soignemoiwebsite/accueil.html'
+
+
+class CreerSejourView(TemplateView):
+    template_name = 'soignemoiwebsite/creer_sejour.html'
