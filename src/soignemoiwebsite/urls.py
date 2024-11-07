bff6653d09036361987ee90ed77e4952d0138faa
zemaf <enaccf31400@gmail.com>
2024-11-07 09:05:47 +0100

    creation vue, url et code js dans le template pour récupérer la liste des médecins en fonction de la spécialité

--- src/soignemoiwebsite/urls.py
+++ src/soignemoiwebsite/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import SoigneMoiWebsiteView, CreerSejourView
-
+from .views import SoigneMoiWebsiteView, get_medecins_par_specialite, creer_sejour
 
 app_name = 'soignemoiwebsite'
 
 urlpatterns = [
     path('accueil/', SoigneMoiWebsiteView.as_view(), name='home'),
-    path('creer_sejour/', CreerSejourView.as_view(), name='creer_sejour'),
+    path('creer_sejour/', creer_sejour, name='creer_sejour'),
+    path('get_medecins/<int:specialite_id>/', get_medecins_par_specialite, name='get_medecins_par_specialite'),
 ]
--- src/soignemoiwebsite/views.py
+++ src/soignemoiwebsite/views.py
@@ -2,10 +2,26 @@ from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from soignemoiwebsite.models import Medecin, Specialite
+
 
 class SoigneMoiWebsiteView(TemplateView):
     template_name = 'soignemoiwebsite/accueil.html'
 
 
-class CreerSejourView(TemplateView):
-    template_name = 'soignemoiwebsite/creer_sejour.html'
+# class CreerSejourView(TemplateView):
+#     template_name = 'soignemoiwebsite/creer_sejour.html'
+
+
+def creer_sejour(request):
+    specialites = Specialite.objects.all()
+    return render(request, 'soignemoiwebsite/creer_sejour.html', {'specialites': specialites})
+
+
+def get_medecins_par_specialite(request, specialite_id):
+    medecins = Medecin.objects.filter(specialite_id=specialite_id).values('id', 'nom', 'prenom')
+    # print(list(medecins))
+    # django s'attendant à un dictionnaire en paramètre de JsonResponse, comme c'est une liste, on lui indique
+    # de s'attendre à autre chose avec safe=False
+    return JsonResponse(list(medecins), safe=False)
+
